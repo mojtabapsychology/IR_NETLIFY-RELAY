94e6e5dbb21ffac70e868df72efb681a76ba37f3
mojtabapsychology <mojtabapsychology@yahoo.com>
2026-05-18 06:38:06 +0330

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+
 const app = express();
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
+  res.send("Render relay online");
+});
+
+app.use((req, res) => {
   res.send("Render relay online");
 });
 
 const port = process.env.PORT || 10000;
 
 app.listen(port, () => {
-  console.log(`Running on ${port}`);
+  console.log(`Running on port ${port}`);
 });
