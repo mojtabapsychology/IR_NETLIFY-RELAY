cfe0e0ecba143911b756fbe2bf6a9aea64a5a15c
mojtabapsychology <mojtabapsychology@yahoo.com>
2026-05-18 06:32:58 +0330

    Create server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("*", (req, res) => {
+  res.send("Render relay online");
+});
+
+const port = process.env.PORT || 10000;
+
+app.listen(port, () => {
+  console.log(`Running on ${port}`);
+});
